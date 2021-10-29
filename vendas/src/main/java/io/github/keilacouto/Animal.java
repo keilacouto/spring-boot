1c255b6fff33a8ecf56c8418b2cccfc841b8686b
Keila Couto <keylacoutos@gmail.com>
2021-10-29 13:51:20 -0300

    Criando annotations customizadas de configuração

---
+++ vendas/src/main/java/io/github/keilacouto/Animal.java
@@ -0,0 +1,5 @@
+package io.github.keilacouto;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- vendas/src/main/java/io/github/keilacouto/MinhaConfiguration.java
+++ vendas/src/main/java/io/github/keilacouto/MinhaConfiguration.java
@@ -1,9 +1,17 @@
 package io.github.keilacouto;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-@Configuration
+@Development
 public class MinhaConfiguration {
 
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
+    }
 }
