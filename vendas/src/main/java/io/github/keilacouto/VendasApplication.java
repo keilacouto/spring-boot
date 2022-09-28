57bb4e50bf65f28b24afb9ea4a118a115601ff94
Keila Couto <keylacoutos@gmail.com>
2022-09-28 11:42:09 -0300

    Concluindo o cadastro de clientes

--- vendas/src/main/java/io/github/keilacouto/VendasApplication.java
+++ vendas/src/main/java/io/github/keilacouto/VendasApplication.java
@@ -16,11 +16,41 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
+
+            System.out.println("Salvando cliente");
             clientes.salvar(new Cliente("Keila"));
             clientes.salvar(new Cliente("Adriano"));
 
+            System.out.println("Listando cliente");
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando cliente");
+            todosClientes.forEach(c ->  {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando cliente por nome");
+            clientes.buscarPorNome("Adri").forEach(System.out::println);
+
+            System.out.println("Deletando cliente por nome");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            System.out.println("Listando cliente");
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()) {
+                System.out.println("Nenhum cliente encontrado.");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+
+
         };
     }
 
