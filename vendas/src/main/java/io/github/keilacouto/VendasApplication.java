c9830358a1b732a6ce26ea6fc95d6a0572c01430
Keila Couto <keylacoutos@gmail.com>
2022-11-09 23:42:18 -0300

    Fazendo consultas com relacionamentos JPA

--- vendas/src/main/java/io/github/keilacouto/VendasApplication.java
+++ vendas/src/main/java/io/github/keilacouto/VendasApplication.java
@@ -1,28 +1,41 @@
 package io.github.keilacouto;
 
 import io.github.keilacouto.domain.entity.Cliente;
+import io.github.keilacouto.domain.entity.Pedido;
 import io.github.keilacouto.domain.repository.Clientes;
+import io.github.keilacouto.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes) {
+    public CommandLineRunner init(
+            @Autowired Clientes clientes,
+            @Autowired Pedidos pedidos) {
         return args -> {
 
             System.out.println("Salvando cliente");
-            clientes.save(new Cliente("Keila"));
-            clientes.save(new Cliente("Adriano"));
+            Cliente keila = new Cliente("Keila");
+            clientes.save(keila);
 
-            List<Cliente> result = clientes.encontrarPorNome("Keila");
-            result.forEach(System.out::println);
+            Pedido p = new Pedido();
+            p.setCliente(keila);
+            p.setDataPedido(LocalDate.now());
+            p.setTotal(BigDecimal.valueOf(100));
+
+            pedidos.save(p);
+
+            Cliente cliente = clientes.findClienteFetchPedidos(keila.getId());
+            System.out.println(cliente.getPedidos());
         };
     }
 
